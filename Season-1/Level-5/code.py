ea6057767032f3eeaf4412ae10a7c8929ef9cc45
Kodikuu <Kodikuu@users.noreply.github.com>
2024-02-17 18:33:04 +0000

    Level 5

--- Season-1/Level-5/code.py
+++ Season-1/Level-5/code.py
@@ -1,58 +1,42 @@
 # Welcome to Secure Code Game Season-1/Level-5!
 
 # This is the last level of our first season, good luck!
-
-import binascii
-import random
-import secrets
-import hashlib
 import os
+import secrets
 import bcrypt
+from argon2 import PasswordHasher
 
 class Random_generator:
 
-    # generates a random token
+    # generates a random token using the secrets library for true randomness
     def generate_token(self, length=8, alphabet=(
     '0123456789'
     'abcdefghijklmnopqrstuvwxyz'
     'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     )):
-        return ''.join(random.choice(alphabet) for _ in range(length))
+        return ''.join(secrets.choice(alphabet) for i in range(length))
 
-    # generates salt
+    # generates salt using the bcrypt library which is a safe implementation
     def generate_salt(self, rounds=12):
-        salt = ''.join(str(random.randint(0, 9)) for _ in range(21)) + '.'
-        return f'$2b${rounds}${salt}'.encode()
+        return bcrypt.gensalt(rounds)
 
-class SHA256_hasher:
+class Hasher:
 
     # produces the password hash by combining password + salt because hashing
     def password_hash(self, password, salt):
-        password = binascii.hexlify(hashlib.sha256(password.encode()).digest())
-        password_hash = bcrypt.hashpw(password, salt)
-        return password_hash.decode('ascii')
+        ph = PasswordHasher()
+        return ph.hash(password, salt=salt)
 
     # verifies that the hashed password reverses to the plain text version on verification
     def password_verification(self, password, password_hash):
-        password = binascii.hexlify(hashlib.sha256(password.encode()).digest())
-        password_hash = password_hash.encode('ascii')
-        return bcrypt.checkpw(password, password_hash)
-
-class MD5_hasher:
-
-    # same as above but using a different algorithm to hash which is MD5
-    def password_hash(self, password):
-        return hashlib.md5(password.encode()).hexdigest()
-
-    def password_verification(self, password, password_hash):
-        password = self.password_hash(password)
-        return secrets.compare_digest(password.encode(), password_hash.encode())
+        ph = PasswordHasher()
+        return ph.verify(password_hash, password)
 
 # a collection of sensitive secrets necessary for the software to operate
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
 PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
-SECRET_KEY = 'TjWnZr4u7x!A%D*G-KaPdSgVkXp2s5v8'
-PASSWORD_HASHER = 'MD5_hasher'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+PASSWORD_HASHER = 'Hasher'
 
 
 # Contribute new levels to the game in 3 simple steps!
